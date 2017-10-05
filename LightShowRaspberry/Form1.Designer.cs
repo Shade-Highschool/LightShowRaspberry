3504c0d4832f93c1ae76a4790fa67255f2a1f828
Tomas <hacker-shade@seznam.cz>
2017-10-05 16:38:21 +0200

    Version 1.0

--- LightShowRaspberry/Form1.Designer.cs
+++ LightShowRaspberry/Form1.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Music on raspberry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Not connected";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(229, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 277);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Play song";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Upload song";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MP3 |*.mp3";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Remove song";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 244);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(521, 307);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LightShow Raspberry Pi";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- LightShowRaspberry/Form1.cs
+++ LightShowRaspberry/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Renci.SshNet;
+using System.IO;
 
 namespace LightShowRaspberry
 {
@@ -15,6 +17,151 @@ namespace LightShowRaspberry
         public Form1()
         {
             InitializeComponent();
+
+            ftpClient = new SftpClient(RaspberryIp, loginName, loginPassword);
+            sshClient = new SshClient(RaspberryIp, loginName, loginPassword);
+        }
+
+        string RaspberryIp = "192.168.0.105";
+
+        string workingDirectory = "/home/pi/Music/";
+        string loginName = "pi";
+        string loginPassword = "raspberry";
+
+        SftpClient ftpClient;
+        SshClient sshClient;
+
+        private void GetMusicVolume()
+        {
+            if (TryConnect(sshClient))
+            {
+                var response = sshClient.RunCommand("awk -F\"[][]\" '/dB/ { print $2 }' <(amixer sget PCM)");
+                string result = response.Result;
+                result = result.Remove(result.Length - 2);
+
+                int value = Convert.ToInt32(result);
+                trackBar1.Value = value;
+
+                sshClient.Disconnect();
+            }
+        }
+
+
+        private void GetMusic()
+        {
+            if (TryConnect(ftpClient))
+            {
+                var fileList = ftpClient.ListDirectory(ftpClient.WorkingDirectory).Where(x => x.Name != ".." && x.Name != ".").Select(s => s.Name);
+
+                listBox1.Items.Clear();
+                foreach (var item in fileList)
+                {
+                    listBox1.Items.Add(item);
+                }
+                label1.Text = "Connection succesful.";
+                ftpClient.Disconnect();
+            }
+            else
+                MessageBox.Show("Cannot connect");
+        }
+
+        private bool TryConnect(SftpClient ftpClient)
+        {
+            try
+            {
+                ftpClient.Connect();
+                ftpClient.ChangeDirectory(workingDirectory);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool TryConnect(SshClient sshClient)
+        {
+            try
+            {
+                sshClient.Connect();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetMusic();
+            GetMusicVolume();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string uploadFile = "";
+            string uploadFileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                uploadFile = openFileDialog1.FileName;
+                uploadFileName = Path.GetFileName(uploadFile);
+            }
+            else
+                return;
+
+            if (TryConnect(ftpClient))
+            {
+                if (!ftpClient.Exists(uploadFileName))
+                {
+                    using (var fileStream = new FileStream(uploadFile, FileMode.Open))
+                    {
+                        ftpClient.BufferSize = 4 * 1024; // bypass Payload error large files
+                        ftpClient.UploadFile(fileStream, uploadFileName);
+                    }
+
+                    listBox1.Items.Add(uploadFileName);
+                }
+                else
+                    MessageBox.Show("Song is already there");
+
+                ftpClient.Disconnect();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (TryConnect(ftpClient))
+            {
+                string listboxSong = listBox1.SelectedItem.ToString();
+                ftpClient.DeleteFile(listboxSong);
+                listBox1.Items.Remove(listboxSong);
+                ftpClient.Disconnect();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                if (TryConnect(sshClient))
+                {
+
+                    ShellStream shellStream = sshClient.CreateShellStream("Tail", 80, 24, 800, 600, 1024);
+                    shellStream.WriteLine("sudo amixer set PCM " + trackBar1.Value + "%");
+                    shellStream.WriteLine("sudo python /home/pi/lightshow/py/synchronized_lights.py --file=/home/pi/Music/\"" + listBox1.SelectedItem.ToString() + "\"");
+                    MessageBox.Show("Song is playing. To stop, close this dialog");
+                    shellStream.Write("\x003");
+                    shellStream.Close();
+                    // var terminal = sshClient.RunCommand("sudo python /home/pi/lightshow/py/synchronized_lights.py --file=/home/pi/Music/" + listBox1.SelectedItem.ToString());
+                    // MessageBox.Show(terminal.Result);
+
+                    sshClient.Disconnect();
+                }
+            }
+            else
+                MessageBox.Show("Song isnt selected");
         }
     }
 }
