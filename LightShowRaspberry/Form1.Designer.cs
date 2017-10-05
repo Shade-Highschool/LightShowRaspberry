6f13166718b7d8ceead020842bf0ace0799e91a6
Zabil <Zabil@LAPTOP-6HQHEH7P>
2017-10-05 18:54:24 +0200

    changed label

--- LightShowRaspberry/Form1.Designer.cs
+++ LightShowRaspberry/Form1.Designer.cs
@@ -43,9 +43,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Location = new System.Drawing.Point(16, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 45);
+            this.button1.Size = new System.Drawing.Size(177, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,26 +55,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.Size = new System.Drawing.Size(74, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Not connected";
+            this.label1.Text = "connected";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(229, 12);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(305, 15);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
             this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBox1.Size = new System.Drawing.Size(267, 277);
+            this.listBox1.Size = new System.Drawing.Size(355, 340);
             this.listBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(131, 114);
+            this.button2.Location = new System.Drawing.Point(175, 140);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "Play song";
             this.button2.UseVisualStyleBackColor = true;
@@ -81,9 +86,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 114);
+            this.button3.Location = new System.Drawing.Point(16, 140);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.Size = new System.Drawing.Size(100, 42);
             this.button3.TabIndex = 4;
             this.button3.Text = "Upload song";
             this.button3.UseVisualStyleBackColor = true;
@@ -97,9 +103,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 154);
+            this.button4.Location = new System.Drawing.Point(16, 190);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 34);
+            this.button4.Size = new System.Drawing.Size(100, 42);
             this.button4.TabIndex = 5;
             this.button4.Text = "Remove song";
             this.button4.UseVisualStyleBackColor = true;
@@ -107,17 +114,19 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 244);
+            this.trackBar1.Location = new System.Drawing.Point(16, 300);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.Size = new System.Drawing.Size(139, 56);
             this.trackBar1.TabIndex = 6;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(131, 194);
+            this.button5.Location = new System.Drawing.Point(175, 239);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(100, 28);
             this.button5.TabIndex = 7;
             this.button5.Text = "Close connection";
             this.button5.UseVisualStyleBackColor = true;
@@ -125,9 +134,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(131, 165);
+            this.button6.Location = new System.Drawing.Point(175, 203);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(100, 28);
             this.button6.TabIndex = 8;
             this.button6.Text = "Stop";
             this.button6.UseVisualStyleBackColor = true;
@@ -135,9 +145,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 307);
+            this.ClientSize = new System.Drawing.Size(695, 378);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.trackBar1);
@@ -147,6 +157,7 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "LightShow Raspberry Pi";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
