78ac55d2d9d0ddf6ea9a979fbefa8c6e326dbd40
Zabil <Zabil@LAPTOP-6HQHEH7P>
2017-10-05 19:33:21 +0200

    test

--- LightShowRaspberry/Form1.cs
+++ LightShowRaspberry/Form1.cs
@@ -12,9 +12,9 @@ using System.IO;
 
 namespace LightShowRaspberry
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
             connection = new SshConnection(RaspberryIp, loginName, loginPassword);
